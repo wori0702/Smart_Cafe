97aac24fc4bd5f28a9a3c176a025e941b8736703
w987987 <w987987@naver.com>
2019-11-28 18:44:41 +0900

    Merge branch 'master' of C:\Users\w9879\Desktop\Smart_Cafe with conflicts.

--- app/src/main/java/com/example/smartcafe/MainActivity.java
+++ app/src/main/java/com/example/smartcafe/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.smartcafe;
 import androidx.appcompat.app.AppCompatActivity;
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import android.content.Intent;
@@ -18,65 +15,57 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
+//테스트 수정
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView T1;
-    public OkHttpClient client;
-    public  Request request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OkHttpClient client = new OkHttpClient();
 
-
-
-        final RequestBody requestBody = new FormBody.Builder()
-                .add("str[0]","20")
-                .add("str[1]","30")
-                .build();
-        Log.d("error","error is " +requestBody);
-
-       client = new OkHttpClient();
-
-        String url = "http://192.168.0.80/getdata";                                 //wifi 모듈에 잡아놓은 수동 IP http://192.168.0.80
-        request = new Request.Builder()
+        String url = "http://192.168.0.80";                                 //wifi 모듈에 잡아놓은 수동 IP
+        final Request request = new Request.Builder()
                 .url(url)
-                .post(requestBody)
                 .build();
+        System.out.println("request : " + request);
 
         Button RoomButton = findViewById(R.id.RoomButton);
         Button SettingButton = findViewById(R.id.MySettingButton);
 
         T1 = findViewById(R.id.BTnum);
-        final TextView T2 = findViewById(R.id.TPnum);
-        final TextView T3 = findViewById(R.id.Co2num);
+        TextView T2 = findViewById(R.id.TPnum);
+        TextView T3 = findViewById(R.id.Co2num);
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
+                Log.d("error","Connect Sever Error is " +call +" _"+e.toString());
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if(response.isSuccessful()){
                     final String myRespone = response.body().string();
-//                    final String[] splittest = myRespone.split("\"");
+
+                    System.out.println("Respone : "+ myRespone);
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             T1.setText(myRespone);
-//                            T2.setText(splittest[1]);
-//                            T3.setText(splittest[2]);
                         }
                     });
                 }
             }
         });
+
     }
+
 
 
     @Override
@@ -84,33 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId())
         {
-
             case R.id.RoomButton:
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        if(response.isSuccessful()){
-                            final String myRespone = response.body().string();
-//                    final String[] splittest = myRespone.split("\"");
-                            System.out.println("Respone : "+ response.toString());
-                            MainActivity.this.runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    T1.setText(myRespone);
-//                            T2.setText(splittest[1]);
-//                            T3.setText(splittest[2]);
-                                }
-                            });
-                        }
-
-                    }
-                });
-
                 Intent intent = new Intent(this, RoomAcivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
                 startActivity(intent);
