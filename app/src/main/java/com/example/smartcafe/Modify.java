2ac704356b52413788e124c27c9ea2447a340879
w987987 <w987987@naver.com>
2019-12-02 00:13:25 +0900

    이미지크기 바꿈

--- app/src/main/java/com/example/smartcafe/Modify.java
+++ app/src/main/java/com/example/smartcafe/Modify.java
@@ -92,9 +92,9 @@ public class Modify extends AppCompatActivity {
                 subll.setPaddingRelative(0, 100, 0, 0);
                 b1.setLayoutParams(btlp);
 
-                if(splitData[0].equals("0")) iv.setImageDrawable(getDrawable(R.drawable.laptop2));
-                if(splitData[0].equals("1")) iv.setImageDrawable(getDrawable(R.drawable.discuss));
-                if(splitData[0].equals("2")) iv.setImageDrawable(getDrawable(R.drawable.study));
+                if(splitData[0].equals("0")) iv.setImageDrawable(getDrawable(R.drawable.laptop3));
+                if(splitData[0].equals("1")) iv.setImageDrawable(getDrawable(R.drawable.discuss3));
+                if(splitData[0].equals("2")) iv.setImageDrawable(getDrawable(R.drawable.study3));
 
                 tv.setText(splitData[1]);
                 tv.setGravity(Gravity.CENTER);
